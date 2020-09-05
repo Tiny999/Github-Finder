46f32faf5c78cb81b53612f1d828cba23842d623
Tiny999 <salamiabbas3@gmail.com>
2020-09-05 18:34:04 +0100

    authentication

--- js/github.js
+++ js/github.js
@@ -9,10 +9,20 @@ class GitHub{
   //  Get User
   async getUser(user){
 
-    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&?client_secret=${this.client_secret}`);
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&?client_secret=${this.client_secret}`,
+    { 
+      headers: {
+        authorization: "token 117bffaeca06ce967264414e9e181ed7ffdd0576"
+      }
+    });
 
     
-    const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repose_sort}&client_id=${this.client_id}&?client_secret=${this.client_secret}`);
+    const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repose_sort}&client_id=${this.client_id}&?client_secret=${this.client_secret}`,
+    {
+      headers: {
+        authorization: "token 117bffaeca06ce967264414e9e181ed7ffdd0576"
+      }
+    });
   
     const profile = await profileResponse.json();
     const repos = await reposResponse.json();
